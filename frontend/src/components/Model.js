8bab5372e33889d4b62c902fb49c0ceb14192d29
Faiyad <ahmedfaiyad7@gmail.com>
2026-03-14 00:05:29 -0400

    wow sky

--- frontend/src/components/Model.js
+++ frontend/src/components/Model.js
@@ -1,11 +1,24 @@
 // components/Model.js
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, useTexture } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+import { useEffect } from 'react';
 
 export default function Model({ url }) {
   // useGLTF handles loading and caching automatically
-  const { scene } = useGLTF(url);
   
-  return <primitive object={scene} scale={1.5} position={[0, 0, 0]} />;
+  const { scene } = useGLTF(url);
+  const { scene: threeScene } = useThree();
+  const bg = useTexture('/images/sky.png');
+  useEffect(() => {
+    threeScene.background = bg;
+  }, [bg, threeScene]);
+  
+  return <>
+   
+    <primitive object={scene} scale={1.5} position={[0, 0, 0]} />;
+  
+  </>
 }
 
 // Pre-loading helps avoid "popping" during navigation
